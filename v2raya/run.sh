2083cf06168e143813d15682750020834e88ab71
zhiheng.wang <zhiheng.wang@foxmail.com>
2026-03-08 15:16:25 +0800

    Revert "更新"
    This reverts commit 18649c1906416bc15ef3a06e410af76f5d9e5081.

--- v2raya/run.sh
+++ v2raya/run.sh
@@ -1,31 +1,42 @@
-#!/usr/bin/env bashio
-
-# 读取配置
-V2RAY_BIN=$(bashio::config 'v2ray_bin')
-NFTABLES_SUPPORT=$(bashio::config 'nftables_support')
-IPTABLES_MODE=$(bashio::config 'iptables_mode')
-LOG_FILE=$(bashio::config 'log_file')
-
-# 设置环境变量
-export V2RAYA_V2RAY_BIN="${V2RAY_BIN}"
-export V2RAYA_LOG_FILE="${LOG_FILE}"
-export V2RAYA_NFTABLES_SUPPORT="${NFTABLES_SUPPORT}"
-export IPTABLES_MODE="${IPTABLES_MODE}"
+#!/bin/sh
 
 # 创建必要的目录
 mkdir -p /etc/v2raya
-mkdir -p /data/v2raya
+mkdir -p /var/log/v2raya
 
-# 如果存在/data/v2raya目录，创建符号链接
-if [ -d /data/v2raya ]; then
-    ln -sf /data/v2raya /etc/v2raya
-fi
+# 生成默认的V2Ray配置文件
+cat > /etc/v2raya/v2ray.json << EOF
+{
+  "log": {
+    "access": "/var/log/v2raya/access.log",
+    "error": "/var/log/v2raya/error.log",
+    "loglevel": "warning"
+  },
+  "inbounds": [
+    {
+      "port": 1080,
+      "protocol": "socks",
+      "settings": {
+        "auth": "noauth",
+        "udp": true
+      }
+    },
+    {
+      "port": 1081,
+      "protocol": "http",
+      "settings": {
+        "auth": "noauth"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ]
+}
+EOF
 
-bashio::log.info "Starting V2rayA..."
-bashio::log.info "V2Ray binary: ${V2RAYA_V2RAY_BIN}"
-bashio::log.info "Log file: ${V2RAYA_LOG_FILE}"
-bashio::log.info "Nftables support: ${V2RAYA_NFTABLES_SUPPORT}"
-bashio::log.info "Iptables mode: ${IPTABLES_MODE}"
-
-# 启动v2rayA
-exec /usr/bin/v2raya
+# 启动supervisord
+exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
