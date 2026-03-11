87c0dab0599f4735c78e9273a70fbe22dbf762da
zhiheng.wang <zhiheng.wang@foxmail.com>
2026-03-11 22:05:46 +0800

    dockercompose

--- v2raya/run.sh
+++ v2raya/run.sh
@@ -1,42 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-# 创建必要的目录
-mkdir -p /etc/v2raya
-mkdir -p /var/log/v2raya
+# 创建数据目录
+mkdir -p ./data
 
-# 生成默认的V2Ray配置文件
-cat > /etc/v2raya/v2ray.json << EOF
-{
-  "log": {
-    "access": "/var/log/v2raya/access.log",
-    "error": "/var/log/v2raya/error.log",
-    "loglevel": "warning"
-  },
-  "inbounds": [
-    {
-      "port": 1080,
-      "protocol": "socks",
-      "settings": {
-        "auth": "noauth",
-        "udp": true
-      }
-    },
-    {
-      "port": 1081,
-      "protocol": "http",
-      "settings": {
-        "auth": "noauth"
-      }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    }
-  ]
-}
-EOF
+# 启动V2rayA服务
+docker-compose up -d
 
-# 启动supervisord
-exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
+# 显示服务状态
+docker-compose ps
